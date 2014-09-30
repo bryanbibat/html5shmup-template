200718841748eefa956318e8f2ac89ecac0bb8f2
Bryan Bibat <bry@bryanbibat.net>
2014-10-01 04:07:53 +0800

    Add OGG to preloader

--- preloader.js
+++ preloader.js
@@ -34,11 +34,11 @@ BasicGame.Preloader.prototype = {
     this.load.spritesheet('boss', 'assets/boss.png', 93, 75);
     this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
     this.load.spritesheet('player', 'assets/player.png', 64, 64);
-    this.load.audio('explosion', ['assets/explosion.wav']);
-    this.load.audio('playerExplosion', ['assets/player-explosion.wav']);
-    this.load.audio('enemyFire', ['assets/enemy-fire.wav']);
-    this.load.audio('playerFire', ['assets/player-fire.wav']);
-    this.load.audio('powerUp', ['assets/powerup.wav']);
+    this.load.audio('explosion', ['assets/explosion.ogg', 'assets/explosion.wav']);
+    this.load.audio('playerExplosion', ['assets/player-explosion.ogg', 'assets/player-explosion.wav']);
+    this.load.audio('enemyFire', ['assets/enemy-fire.ogg', 'assets/enemy-fire.wav']);
+    this.load.audio('playerFire', ['assets/player-fire.ogg', 'assets/player-fire.wav']);
+    this.load.audio('powerUp', ['assets/powerup.ogg', 'assets/powerup.wav']);
     //this.load.audio('titleMusic', ['audio/main_menu.mp3']);
     //  + lots of other required assets here
 

54991f47773adff5bfa1a128fbe404d9405af57a
Bryan Bibat <bry@bryanbibat.net>
2014-12-22 14:44:19 +0800

    Finally make the template work

--- src/js/game/app.js
+++ src/js/game/app.js
@@ -8,20 +8,14 @@ var BasicGame = {
   Game: require('./states/game.js')
 };
 
-window.onload = function() {
+var game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'gameContainer');
 
-  //  Create your Phaser game and inject it into the gameContainer div.
-  //  We did it in a window.onload event, but you can do it anywhere (requireJS load, anonymous function, jQuery dom ready, - whatever floats your boat)
-  var game = new Phaser.Game(properties.size.x, properties.size.y, Phaser.AUTO, 'gameContainer');
+//  Add the States your game has.
+//  You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
+game.state.add('Boot', BasicGame.Boot);
+game.state.add('Preloader', BasicGame.Preloader);
+game.state.add('MainMenu', BasicGame.MainMenu);
+game.state.add('Game', BasicGame.Game);
 
-  //  Add the States your game has.
-  //  You don't have to do this in the html, it could be done in your Boot state too, but for simplicity I'll keep it here.
-  game.state.add('Boot', BasicGame.Boot);
-  game.state.add('Preloader', BasicGame.Preloader);
-  game.state.add('MainMenu', BasicGame.MainMenu);
-  game.state.add('Game', BasicGame.Game);
-
-  //  Now start the Boot state.
-  game.state.start('Boot');
-
-};
+//  Now start the Boot state.
+game.state.start('Boot');
